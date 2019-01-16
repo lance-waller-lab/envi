1c960782a8d45902505fa3b2ed7728ac1ef6833f
idblr <ian.d.buller@gmail.com>
2019-01-15 20:14:00 -0500

    Reorganized PCA before masking to western US

--- code/test_jitter_llren.R
+++ code/test_jitter_llren.R
@@ -70,7 +70,7 @@ prism::get_prism_normals(type= "tdmean"
 
 # Convert to Rasters
 ppt <- ls_prism_data(absPath=T)[1,2]
-ppt_rast <- raster(ppt)
+ppt_rast <- raster::raster(ppt)
 tdmean <- ls_prism_data(absPath=T)[2,2]
 tdmean_rast <- raster(tdmean)
 tmax <- ls_prism_data(absPath=T)[3,2]
@@ -107,18 +107,10 @@ scaled_reproj_vpdmin_rast <-(reproj_vpdmin_rast-min(na.omit(reproj_vpdmin_rast@d
 library(RStoolbox)
 #library(raster)
 
-# MASK to WUS
-mask_ppt_rast_scale <- mask(scaled_reproj_ppt_rast,wus_buffer)
-mask_tdmean_rast_scale <- mask(scaled_reproj_tdmean_rast,wus_buffer)
-mask_tmax_rast_scale <- mask(scaled_reproj_tmax_rast,wus_buffer)
-mask_tmean_rast_scale <- mask(scaled_reproj_tmean_rast,wus_buffer)
-mask_tmin_rast_scale <- mask(scaled_reproj_tmin_rast,wus_buffer)
-mask_vpdmax_rast_scale <- mask(scaled_reproj_vpdmax_rast,wus_buffer)
-mask_vpdmin_rast_scale <- mask(scaled_reproj_vpdmin_rast,wus_buffer)
-
 # Raster Stack for PCA
-mask_rasters_scaled <- stack(mask_ppt_rast_scale, mask_tdmean_rast_scale,
-                             mask_tmax_rast_scale,mask_tmean_rast_scale,mask_tmin_rast_scale,mask_vpdmax_rast_scale,mask_vpdmin_rast_scale)
+mask_rasters_scaled <- stack(scaled_reproj_ppt_rast, scaled_reproj_tdmean_rast,
+                             scaled_reproj_tmax_rast,scaled_reproj_tmean_rast,
+                             scaled_reproj_tmin_rast,scaled_reproj_vpdmax_rast,scaled_reproj_vpdmin_rast)
 
 # PCA (rasters masked by window)
 pca1 <- rasterPCA(mask_rasters_scaled)
@@ -131,17 +123,21 @@ pc1_b1 <- pc1[[1]] # PC1
 pc1_b2 <- pc1[[2]] # PC2
 #pc1_b3 <- pc1[[3]] # PC3
 
+# MASK to WUS
+pc1_b1_mask <- mask(pc1_b1, wus)
+pc1_b2_mask <- mask(pc1_b2, wus)
+
 # Scale Up so minimum value is less than or equal to 0 for K&D test to work
-pc1_scaled <- (pc1_b1 + abs(pc1_b1@data@min))
-pc2_scaled <- (pc1_b2 + abs(pc1_b2@data@min))
+pc1_scaled <- (pc1_b1_mask + abs(pc1_b1_mask@data@min))
+pc2_scaled <- (pc1_b2_mask + abs(pc1_b2_mask@data@min))
 #pc3_scaled <- pc1_b3 + abs(pc1_b3@data@min)
 
 # Western US Extraction Points
-extract_points_wus <- rasterToPoints(pc1_b2)
+extract_points_wus <- raster::rasterToPoints(pc1_scaled)
 extract_points_wus <- extract_points_wus[,1:2]
 predict_locs <- data.frame(coordinates(extract_points_wus), raster::extract(pc1_scaled,extract_points_wus), raster::extract(pc2_scaled,extract_points_wus))
 names(predict_locs) <- c("lon", "lat", "pc1_scaled", "pc2_scaled")
-
+summary(predict_locs[,3])
 
 ## Observation Data
 usda_coyote_raw <- read.csv("/Users/IDB/Box Sync/Dissertation/Data/US_COYOTE_ANALYSIS/Buller_coyote plague_data_latlong_V5.csv")
@@ -163,7 +159,7 @@ names(sim_dat) <- c("id", "lon", "lat", "mark", "levels")
 str(sim_dat)
 
 # Restructure for jitter_KnD function
-raster_dat <- stack(pc1_scaled, pc1_scaled)
+raster_dat <- stack(pc1_scaled, pc2_scaled)
 
 # matrix of distances for each level (levels x 2 matrix)
 distances_mat <- matrix(data = c(500,0,2000,0,6000,0), nrow = 3, ncol = 2, byrow = T)
@@ -176,6 +172,7 @@ sim_num <- 3
 
 
 ## Function Run
+test_jitterKnD <- NULL
 test_jitterKnD <- jitterKnD(data = sim_dat, rasters = raster_dat, sim = sim_num, distances = distances_mat, crs = crs_wgs84)
 str(test_jitterKnD)
 sim_dat_lrren <- test_jitterKnD[[2]]
@@ -189,4 +186,3 @@ points(predict_locs[,1], predict_locs[,2], col = "black", pch = 15, cex = 0.2)
 ## Function Run
 source("code/R_functions/jitter_lrren.R")
 test_jitter_llren <- jitter_llren(sim_locs = sim_dat_lrren, predict_locs = predict_locs, predict = T, plot = T, tolerate = T, adapt = F, resolution = 200)
-
